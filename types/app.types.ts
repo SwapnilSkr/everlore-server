adabb40612c85fa37ec82c400f5299edcd1d927b
SwapnilSkr <swapnilmkab@gmail.com>
2025-12-10 16:00:52 +0530

    more optimal type bindings

--- types/app.types.ts
+++ types/app.types.ts
@@ -1,19 +1,32 @@
 import { ApiRouteConfig, EventConfig, CronConfig } from "motia";
-import type { Emit } from "motia";
 
-type Optional<T, K extends keyof T> = Omit<T, K> & Pick<Partial<T>, K>;
+type RequiredFieldKeys = "name" | "type" | "description";
 
-export type TypedApiRouteConfig = Optional<ApiRouteConfig, "emits">;
+type WithOptionalEmits<T> = T extends { emits: infer E }
+  ? Omit<T, "emits"> & { emits?: E }
+  : T;
+type RequiredFields<T, K extends keyof T> = T & {
+  [P in K]-?: Exclude<T[P], undefined>;
+};
 
-export type TypedEventConfig = Optional<EventConfig, "emits">;
-
-export type TypedCronConfig = Optional<CronConfig, "emits">;
+export type TypedApiRouteConfig = RequiredFields<
+  WithOptionalEmits<ApiRouteConfig>,
+  RequiredFieldKeys
+>;
+export type TypedEventConfig = RequiredFields<
+  WithOptionalEmits<EventConfig>,
+  RequiredFieldKeys
+>;
+export type TypedCronConfig = RequiredFields<
+  WithOptionalEmits<CronConfig>,
+  RequiredFieldKeys
+>;
 
 export function normalizeConfig(
   config: TypedApiRouteConfig | TypedEventConfig | TypedCronConfig
 ): ApiRouteConfig | EventConfig | CronConfig {
   return {
     ...config,
-    emits: config.emits || ([] as Emit[]),
-  } as ApiRouteConfig | EventConfig | CronConfig;
+    emits: config.emits ?? [],
+  };
 }
